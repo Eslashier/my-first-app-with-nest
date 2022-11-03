2ac4616d4bbccc86d907866897936a1156ffa3bf
Eslashier <anfelov@gmail.com>
2022-11-03 18:42:00 -0500

    getHello modofied to get a different message

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Este es mi primera aplicación en Nest';
+  getMyFirstService(): string {
+    return 'Esta es mi primera aplicación en Nest';
   }
 }
